337dfaeca48a41b7d55dfeec4e3111a3e2d25915
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-10-22 18:31:24 -0300

    refactor: validate product stock

--- ms-rails/app/models/product.rb
+++ ms-rails/app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :name,  length: {minimum:4}
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000}
   validates :brand, :description, presence: true
+  validates :stock, numericality:{only_integer:true}
 end
